b09cdf425c51e642aa06dfcd999b572e5ea341cc
Lucas Basto <136653403+luccabasto@users.noreply.github.com>
2025-05-22 23:48:40 -0300

    Update CadastroController.java

--- src/main/java/com/example/odontogenda/controllers/CadastroController.java
+++ src/main/java/com/example/odontogenda/controllers/CadastroController.java
@@ -1,58 +1,37 @@
 package com.example.odontogenda.controllers;
 
-import com.example.odontogenda.exceptions.InvalidUserPrefixException;
 import com.example.odontogenda.models.Cliente;
 import com.example.odontogenda.models.Dentista;
+import com.example.odontogenda.messaging.producer.UsuarioProducer;
 import com.example.odontogenda.services.ClienteService;
 import com.example.odontogenda.services.DentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/cadastro")
 public class CadastroController {
 
-    @Autowired
-    private ClienteService clienteService;
-
-    @Autowired
-    private DentistaService dentistaService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @GetMapping("/cliente")
-    public String formCliente(Model model, @RequestParam(value="error", required = false) String error) {
-        model.addAttribute("cliente", new Cliente());
-        if (error != null) {
-            model.addAttribute("error", error);
-        }
-        return "cadastroCliente";
-    }
+    @Autowired private ClienteService clienteService;
+    @Autowired private DentistaService dentistaService;
+    @Autowired private PasswordEncoder passwordEncoder;
+    @Autowired private UsuarioProducer producer;
 
     @PostMapping("/cliente")
     public String cadastrarCliente(@ModelAttribute Cliente cliente) {
         cliente.setSenha(passwordEncoder.encode(cliente.getSenha()));
-        clienteService.save(cliente);
+        Cliente salva = clienteService.save(cliente);
+        producer.sendUsuarioMessage(salva);
         return "redirect:/login";
-    }
-
-    @GetMapping("/dentista")
-    public String formDentista(Model model, @RequestParam(value="error", required = false) String error) {
-        model.addAttribute("dentista", new Dentista());
-        if (error != null) {
-            model.addAttribute("error", error);
-        }
-        return "cadastroDentista";
     }
 
     @PostMapping("/dentista")
     public String cadastrarDentista(@ModelAttribute Dentista dentista) {
         dentista.setSenha(passwordEncoder.encode(dentista.getSenha()));
-        dentistaService.save(dentista);
+        Dentista salva = dentistaService.save(dentista);
+        producer.sendUsuarioMessage(salva);
         return "redirect:/login";
     }
 }
